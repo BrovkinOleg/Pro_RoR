3a33b23dee74a2d7137f0a575f50a4ac17317334
Brovkin Oleg <boxline@yandex.ru>
2019-11-02 11:04:25 +0300

    "test 'GET #show' is ready - green"

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,17 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = Question.new
+    if @question
+      render new_question_path
+    else
+      render edit_question_path
+    end
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,16 +3,31 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
-
     before { get :index }
 
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
-
     it 'renders index view' do
       expect(response).to render_template :index
     end
   end
 
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+    before { get :show, params: { id: question } }
+
+    it 'assign requested question to @question' do
+      expect(assigns(:question)).to eq(question)
+    end
+    it 'render show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  # describe 'POST #create' do
+  #
+  #   it 'user can create question'
+  #   it 'render created question view'
+  # end
 end
