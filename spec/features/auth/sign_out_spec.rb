591057153644377bb5bdd1afda75f4f590704be1
Brovkin Oleg <boxline@yandex.ru>
2019-11-05 19:23:16 +0300

    "auth spec pass without errors"

--- spec/features/auth/sign_out_spec.rb
+++ spec/features/auth/sign_out_spec.rb
@@ -5,9 +5,9 @@ feature 'User can log out' do
   given(:user) { create :user }
 
   scenario 'Registered and authorized user try to log out' do
-    log_in(user)
+    sign_in(user)
     visit root_path
-    click_on 'Log out'
-    expect(page).to have_content 'Log out successfully.'
+    click_on 'Sign out'
+    expect(page).to have_content 'Signed out successfully.'
   end
 end
