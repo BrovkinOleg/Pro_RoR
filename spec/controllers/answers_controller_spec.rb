bf5c21d3d2dc95977ae66434e13ccc4fb13f07eb
Brovkin Oleg <boxline@yandex.ru>
2019-11-15 09:15:05 +0300

    "change spec for render in answers_controller"

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe AnswersController, type: :controller do
 
         it 'redirect to the questions#index' do
           post :create, params: { answer: attributes_for(:answer), question_id: question }
-          expect(response).to redirect_to assigns(:question)
+          expect(response).to render_template 'questions/show'
           expect(flash[:notice]).to eq 'Your answer successfully created.'
         end
       end
