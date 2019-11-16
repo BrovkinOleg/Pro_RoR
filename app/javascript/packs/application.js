1998dfdb034be63462eeb3c4df54d315cec64b88
Brovkin Oleg <boxline@yandex.ru>
2019-11-16 18:25:59 +0300

    "remove gem 'jquery-rails'"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-// require("custom/answers.js")
-// require("custom/questions.js")
+require("custom/answers.js")
+require("custom/questions.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
