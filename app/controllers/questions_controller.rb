a73fbcd9b6fcd8d15a5d9c4fe2c557e648ef0e5b
Brovkin Oleg <boxline@yandex.ru>
2019-12-03 12:51:31 +0300

    "user can see his profit image"

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,7 +21,6 @@ class QuestionsController < ApplicationController
 
   def create
     @question = current_user.questions.new(question_params)
-    #byebug
     if @question.save
       redirect_to @question, notice: 'Your question successfully created.'
     else
