a1e7f4d51df795d7dae9c8951c3f88f187404832
BrovkinOleg1961 <boxline@yandex.ru>
2019-12-08 06:10:22 -0800

    "add module LinksAssociations"

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,15 +1,14 @@
 
 class Question < ApplicationRecord
+  include LinksAssociations
 
   has_many :answers, dependent: :destroy
-  has_many :links, dependent: :destroy, as: :linkable
   has_one :profit, dependent: :destroy
 
   belongs_to :user
 
   has_many_attached :files
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :profit, reject_if: :all_blank, allow_destroy: true
 
   validates :title, :body, presence: true
