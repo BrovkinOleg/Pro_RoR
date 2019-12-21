7b61f6c68d7415e8ef073921f90e257df4f911d7
BrovkinOleg1961 <boxline@yandex.ru>
2019-12-21 01:41:12 -0800

    "fix bugs in feature_questions_channel_spec"

--- app/javascript/channels/comments_channel.js
+++ app/javascript/channels/comments_channel.js
@@ -1,24 +1,22 @@
 import consumer from "./consumer"
 
-$(document).on('turbolinks:load', function () {
-    consumer.subscriptions.create({
-        channel: "CommentsChannel",
-        question_id: gon.question_id
-    }, {
-        connected() {},
-        disconnected() {},
-        received(data) {
-            let comment = JSON.parse(data);
-            if (gon.user_id === comment.user_id) { return; }
+consumer.subscriptions.create({ channel:"CommentsChannel", question_id: gon.question_id }, {
 
-            let $newCommentDiv = $("<div id=" + "comment_" + comment.id + "></div>");
-            if (comment.commentable_type === 'Question') {
-                $('#comments_question_' + comment.commentable_id).append($newCommentDiv);
-                $($newCommentDiv).append("<b>Comment:</b>", "<p>" + comment.body + "</p>")
-            } else if (comment.commentable_type === 'Answer') {
-                $('#comments_answer_' + comment.commentable_id).append($newCommentDiv);
-                $($newCommentDiv).append("<b>Comment:</b>", "<p>" + comment.body + "</p>")
-            }
+    connected() {},
+    disconnected() {},
+    received(data) {
+        let comment = JSON.parse(data);
+        console.log('comment_receive_data', data);
+        if (gon.user_id === comment.user_id) {
+            return;
         }
-    });
+        let $newCommentDiv = $("<div id=" + "comment_" + comment.id + "></div>");
+        let $name = comment.commentable_type.toLowerCase(); //  $name = 'question' or 'answer'
+
+        console.log('$newCommentDiv = ', $newCommentDiv);
+        console.log('$remote_comment_name = ', $name);
+        $('#comments_'+ $name + '_' + comment.commentable_id).append($newCommentDiv);
+        $($newCommentDiv).append("<b>Remote_Comment:</b>", comment.body);
+    }
 });
+
--- app/javascript/channels/questions_channel.js
+++ app/javascript/channels/questions_channel.js
@@ -1,17 +1,15 @@
 import consumer from "./consumer"
 
-$(document).on('turbolinks:load', function () {
-    consumer.subscriptions.create("QuestionsChannel", {
-        connected() {},
-        disconnected() {},
-        received(data) {
-            let question = JSON.parse(data);
-            if (gon.user_id === question.user_id) {
-                return;
-            }
-            let $newQuestionDiv = $("<div id=" + "remote_question_" + question.id + "> <hr> </div>");
-            $('.questions').append($newQuestionDiv);
-            $($newQuestionDiv).append("<p>" + question.title + "</p>", "<p>" + question.body + "</p>")
+consumer.subscriptions.create("QuestionsChannel", {
+    connected() {},
+    disconnected() {},
+    received(data) {
+        let question = JSON.parse(data);
+        if (gon.user_id === question.user_id) {
+            return;
         }
-    });
+        let $newQuestionDiv = $("<div id=" + "question_" + question.id + "> <hr> </div>");
+        $('.questions').append($newQuestionDiv);
+        $($newQuestionDiv).append("<p>" + question.title + "</p>", "<p>" + question.body + "</p>")
+    }
 });
