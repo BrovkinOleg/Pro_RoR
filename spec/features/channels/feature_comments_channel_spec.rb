6d09f762c0940aed26bdadf6bae1bf62966b4db2
BrovkinOleg1961 <boxline@yandex.ru>
2019-12-21 12:32:56 -0800

    "add feature_comments_channel_spec"

--- spec/features/channels/feature_comments_channel_spec.rb
+++ spec/features/channels/feature_comments_channel_spec.rb
@@ -9,15 +9,13 @@ feature 'User can see in real time appearance of new comments' do
     background do
       Capybara.using_session('first_user') do
         sign_in(user)
-        #visit question_path(question)
-        visit root_path
+        visit question_path(question)
         expect(page).to have_no_content 'my comment'
         click_on 'Add comment to that question'
       end
 
       Capybara.using_session('second_user') do
-        #visit question_path(question)
-        visit root_path
+        visit question_path(question)
         expect(page).to have_no_content 'my comment'
       end
     end
@@ -33,73 +31,73 @@ feature 'User can see in real time appearance of new comments' do
       end
 
       Capybara.using_session('second_user') do
-        #expect(page).to have_selector('div', id: "comment_#{question.comments.first.id}")
+        expect(page).to have_selector('div', id: "comment_#{question.comments.first.id}")
         expect(page).to have_content 'my comment'
       end
     end
 
-    #scenario 'with errors does not appear on another user page' do
-    #  Capybara.using_session('first_user') do
-    #    within "div#question_#{question.id}" do
-    #      click_on 'create comment'
-    #
-    #      expect(page).to have_content "Body can't be blank"
-    #    end
-    #  end
-    #
-    #  Capybara.using_session('second_user') do
-    #    within "div#comments_question_#{question.id}" do
-    #      expect(page).to have_no_selector('div', id: /comment_*/)
-    #    end
-    #  end
-    #end
+    scenario 'with errors does not appear on another user page' do
+      Capybara.using_session('first_user') do
+        within "div#question_#{question.id}" do
+          click_on 'create comment'
+
+          expect(page).to have_content "Body can't be blank"
+        end
+      end
+
+      Capybara.using_session('second_user') do
+        within "div#comments_question_#{question.id}" do
+          expect(page).to have_no_selector('div', id: /comment_*/)
+        end
+      end
+    end
   end
 
-  #describe 'new comment for answer', js: true do
-  #  background do
-  #    Capybara.using_session('first_user') do
-  #      sign_in(user)
-  #      visit question_path(question)
-  #      expect(page).to have_no_content 'my comment'
-  #      click_on 'Add comment to that answer'
-  #    end
-  #
-  #    Capybara.using_session('second_user') do
-  #      visit question_path(question)
-  #      expect(page).to have_no_content 'my comment'
-  #    end
-  #  end
+  describe 'new comment for answer', js: true do
+    background do
+      Capybara.using_session('first_user') do
+        sign_in(user)
+        visit question_path(question)
+        expect(page).to have_no_content 'my comment'
+        click_on 'Add comment to that answer'
+      end
 
-    #scenario 'appears on another user page' do
-    #  Capybara.using_session('first_user') do
-    #    within "div#answer_#{answer.id}" do
-    #      fill_in 'Body', with: 'my comment'
-    #      click_on 'create comment'
-    #    end
-    #
-    #    expect(page).to have_content 'my comment'
-    #  end
-    #
-    #  Capybara.using_session('second_user') do
-    #    expect(page).to have_selector('div', id: "comment_#{answer.comments.first.id}")
-    #    expect(page).to have_content 'my comment'
-    #  end
-    #end
+      Capybara.using_session('second_user') do
+        visit question_path(question)
+        expect(page).to have_no_content 'my comment'
+      end
+    end
 
-    #scenario 'with errors does not appear on another user page' do
-    #  Capybara.using_session('first_user') do
-    #    within "div#answer_#{answer.id}" do
-    #      click_on 'create comment'
-    #
-    #      expect(page).to have_content "Body can't be blank"
-    #    end
-    #  end
-    #
-    #  Capybara.using_session('second_user') do
-    #    within "div#comments_answer_#{answer.id}" do
-    #      expect(page).to have_no_selector('div', id: /comment_*/)
-    #    end
-    #  end
-    #end
-  #end
+    scenario 'appears on another user page' do
+      Capybara.using_session('first_user') do
+        within "div#answer_#{answer.id}" do
+          fill_in 'Body', with: 'my comment'
+          click_on 'create comment'
+        end
+
+        expect(page).to have_content 'my comment'
+      end
+
+      Capybara.using_session('second_user') do
+        expect(page).to have_selector('div', id: "comment_#{answer.comments.first.id}")
+        expect(page).to have_content 'my comment'
+      end
+    end
+
+    scenario 'with errors does not appear on another user page' do
+      Capybara.using_session('first_user') do
+        within "div#answer_#{answer.id}" do
+          click_on 'create comment'
+
+          expect(page).to have_content "Body can't be blank"
+        end
+      end
+
+      Capybara.using_session('second_user') do
+        within "div#comments_answer_#{answer.id}" do
+          expect(page).to have_no_selector('div', id: /comment_*/)
+        end
+      end
+    end
+  end
 end
