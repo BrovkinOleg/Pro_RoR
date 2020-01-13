77c086f4b462dcaa538b5749f587b5e82a47e703
BrovkinOleg1961 <boxline@yandex.ru>
2020-01-12 23:40:13 -0800

    "add abilities to _answer.html.slim"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     gon.user_id = current_user&.id
   end
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+
 end
