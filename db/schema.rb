df743cb55f2e0acb635cd96173af265565cf2394
BrovkinOleg1961 <boxline@yandex.ru>
2020-01-19 07:15:53 -0800

    "add model CreateSubscribers "

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_075339) do
+ActiveRecord::Schema.define(version: 2020_01_19_151342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,16 @@ ActiveRecord::Schema.define(version: 2020_01_16_075339) do
     t.integer "user_id"
   end
 
+  create_table "subscribers", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_subscribers_on_question_id"
+    t.index ["user_id", "question_id"], name: "index_subscribers_on_user_id_and_question_id", unique: true
+    t.index ["user_id"], name: "index_subscribers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -175,5 +185,7 @@ ActiveRecord::Schema.define(version: 2020_01_16_075339) do
   add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
   add_foreign_key "profits", "questions"
   add_foreign_key "profits", "users"
+  add_foreign_key "subscribers", "questions"
+  add_foreign_key "subscribers", "users"
   add_foreign_key "votes", "users"
 end
