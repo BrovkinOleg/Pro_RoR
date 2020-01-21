212454faa30f9f344279efa1cbda790c6dce8293
BrovkinOleg1961 <boxline@yandex.ru>
2020-01-21 08:01:29 -0800

    "notify_new_answer_job"

--- spec/mailers/previews/daily_digest_preview.rb
+++ spec/mailers/previews/daily_digest_preview.rb
@@ -1,7 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/daily_digest
 class DailyDigestPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/daily_digest/digest
   def digest(user)
     DailyDigestMailer.digest(user)
   end
