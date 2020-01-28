81270ae7849681ea1874514021e0f3b066cb8e5e
BrovkinOleg1961 <boxline@yandex.ru>
2020-01-27 22:31:28 -0800

    "add SearchService and SearchController"

---
+++ app/services/search_service.rb
@@ -0,0 +1,10 @@
+class SearchService
+
+  RESOURCES = %w[Question Answer Comment User]
+
+  def self.call(query:, resource: nil)
+    klass = RESOURCES.include?(resource) ? resource.constantize : ThinkingSphinx
+    escaped_query = ThinkingSphinx::Query.escape(query)
+    klass.search escaped_query
+  end
+end
