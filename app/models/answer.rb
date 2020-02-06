46adcca391cff5f31fc63b716863bdd79c143213
BrovkinOleg1961 <boxline@yandex.ru>
2020-02-06 19:40:55 +0300

    "add touch: true to models answer, comment, link, vote"

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ApplicationRecord
   include Votable
   include HasComments
 
-  belongs_to :question
+  belongs_to :question, touch: true
   belongs_to :user
 
   has_many_attached :files
